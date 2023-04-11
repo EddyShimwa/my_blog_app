b377af4b501bccf945a6a57096164768cd6f3e3e
Shimwa Pray Eddy <shedpray16@gmail.com>
2023-04-11 19:46:56 +0200

    added missing changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  def index; end
+
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  def index; end
+
   def show
     @user = User.find(params[:id])
   end
