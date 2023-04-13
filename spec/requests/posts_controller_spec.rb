5b4a82ee055ea53221e25707b0e26ae346266423
Shimwa Pray Eddy <shedpray16@gmail.com>
2023-04-13 11:10:38 +0200

    I added the specs for posts

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -1,1 +1,39 @@
-# for posts_controller
+require 'rails_helper'
+
+RSpec.describe '/posts', type: :request do
+  describe 'GET /index' do
+    before(:example) do
+      get '/users/1/posts'
+    end
+
+    it 'renders a successful response' do
+      expect(response).to be_successful
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template('index')
+    end
+
+    it 'renders the correct body content' do
+      expect(response.body).to include('Here is a list of posts for a given user')
+    end
+  end
+
+  describe 'GET /show' do
+    before(:example) do
+      get '/users/1/posts/1'
+    end
+
+    it 'renders a successful response' do
+      expect(response).to be_successful
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template('show')
+    end
+
+    it 'renders the correct body content' do
+      expect(response.body).to include('Hello, this is post id')
+    end
+  end
+end
