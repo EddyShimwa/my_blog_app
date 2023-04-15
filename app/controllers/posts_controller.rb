aec3fc10d8fcf60714b30d98cc7b8b22af6b730b
Shimwa Pray Eddy <shedpray16@gmail.com>
2023-04-15 05:08:19 +0200

    Added the forms to my app

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,34 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
+    @comment = Comment.new
   end
 
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find_by_id(params[:id])
     @comments = @post.comments
+    @like = Like.new
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to root_path notice: 'Post created successfully'
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
