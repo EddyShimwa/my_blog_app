74792c7e683c54e36a3600ebd7cb99dc3e950b49
EddyShimwa <shedpray16@gmail.com>
2023-04-19 11:38:05 +0200

    fixed linter errors

--- spec/views/post_index_spec.rb
+++ spec/views/post_index_spec.rb
@@ -1,85 +1,85 @@
 require 'rails_helper'
 
 RSpec.describe 'post/index', type: :feature do
-    before(:each) do
-        @user= User.create!(
-            name: 'username1',
-            photo: 'Photo1',
-            bio: 'Text1',
-            posts_counter: 2,
-            )
-        @post_1 = Post.create!(
-            title: 'Title1',
-            text: 'Text1',
-            comments_counter: 3,
-            likes_counter: 0,
-            author: @user
-            )
-        @post_2 = Post.create!(
-            title: 'Title2',
-            text: 'Text2',
-            comments_counter: 0,
-            likes_counter: 0,
-            author: @user
-            )
-        @comment_1 = Comment.create!(
-            text: 'Comment1',
-            post: @post_1,
-            author: @user
-            )
-        @comment_2 = Comment.create!(
-            text: 'Comment2',
-            post: @post_1,
-            author: @user
-            )
-        @comment_3 = Comment.create!(
-            text: 'Comment3',
-            post: @post_1,
-            author: @user
-            )
-        visit user_posts_path(@user.id)
-    end
+  before(:each) do
+    @user = User.create!(
+      name: 'username1',
+      photo: 'Photo1',
+      bio: 'Text1',
+      posts_counter: 2
+    )
+    @post1 = Post.create!(
+      title: 'Title1',
+      text: 'Text1',
+      comments_counter: 3,
+      likes_counter: 0,
+      author: @user
+    )
+    @post2 = Post.create!(
+      title: 'Title2',
+      text: 'Text2',
+      comments_counter: 0,
+      likes_counter: 0,
+      author: @user
+    )
+    @comment1 = Comment.create!(
+      text: 'Comment1',
+      post: @post1,
+      author: @user
+    )
+    @comment2 = Comment.create!(
+      text: 'Comment2',
+      post: @post1,
+      author: @user
+    )
+    @comment3 = Comment.create!(
+      text: 'Comment3',
+      post: @post1,
+      author: @user
+    )
+    visit user_posts_path(@user.id)
+  end
 
-    it 'Show the profile picture' do
-        expect(page).to have_xpath("//img[@src='Photo1']")
-    end
+  it 'Show the profile picture' do
+    expect(page).to have_xpath("//img[@src='Photo1']")
+  end
 
-    it 'Show the username' do
-        expect(page).to have_content('username1')
-    end
+  it 'Show the username' do
+    expect(page).to have_content('username1')
+  end
 
-    it 'Show the number of posts written by the user' do
-        expect(page).to have_content('2')
-    end
+  it 'Show the number of posts written by the user' do
+    expect(page).to have_content('2')
+  end
 
-    it 'Show the post title' do
-        expect(page).to have_content('Title1')
-        expect(page).to have_content('Title2')
-    end
+  it 'Show the post title' do
+    expect(page).to have_content('Title1')
+    expect(page).to have_content('Title2')
+  end
 
-    it 'Show the post body' do
-        expect(page).to have_content('Text1')
-        expect(page).to have_content('Text2')
-    end
+  it 'Show the post body' do
+    expect(page).to have_content('Text1')
+    expect(page).to have_content('Text2')
+  end
 
-    it 'Show the first comment on a post' do
-        expect(page).to have_content('Comment1')
-    end
+  it 'Show the first comment on a post' do
+    expect(page).to have_content('Comment1')
+  end
 
-    it 'Show the number of comments on a post' do
-        expect(page).to have_content('3')
-    end
+  it 'Show the number of comments on a post' do
+    expect(page).to have_content('3')
+  end
 
-    it 'Show the number of likes on a post' do
-        expect(page).to have_content('0')
-    end
+  it 'Show the number of likes on a post' do
+    expect(page).to have_content('0')
+  end
 
-    it 'Show the button Pagination' do
-        expect(page).to have_content('Pagination')
-    end
+  it 'Show the button Pagination' do
+    expect(page).to have_content('Pagination')
+  end
 
-    it 'When the user clicks on the post title, it redirects to the post`s page' do
-        click_on 'Title1'
-        expect(page).to have_current_path user_post_path(@post_1.author_id, @post_1)
-    end
+  it 'When the user clicks on the post title, it redirects to the post`s page' do
+    click_on 'Title1'
+    expect(page).to have_current_path user_post_path(@post1.author_id, @post1)
+  end
 end
--- spec/views/post_show_spec.rb
+++ spec/views/post_show_spec.rb
@@ -1,79 +1,79 @@
 require 'rails_helper'
 
 RSpec.describe 'posts/show', type: :feature do
-    before(:each) do
-        @user_1 = User.create!(
-            name: 'username1',
-            photo: 'Photo',
-            bio: 'Text',
-            posts_counter: 2
-        )
-        @user_2 = User.create!(
-            name: 'username2',
-            photo: 'Photo2',
-            bio: 'Text2',
-            posts_counter: 0,
-        )
-        @post_1 = Post.create!(
-            title: 'Title1',
-            text: 'Text1',
-            comments_counter: 3,
-            likes_counter: 1,
-            author: @user_1
-        )
-        @post_2 = Post.create!(
-            title: 'Title2',
-            text: 'Text2',
-            comments_counter: 0,
-            likes_counter: 0,
-            author: @user_1
-        )
-        @comment_1 = Comment.create!(
-            text: 'Comment1',
-            post: @post_1,
-            author: @user_2
-        )
-        @comment_2 = Comment.create!(
-            text: 'Comment2',
-            post: @post_1,
-            author: @user_2
-        )
-        @comment_3 = Comment.create!(
-            text: 'Comment3',
-            post: @post_1,
-            author: @user_2
-        )
+  before(:each) do
+    @user1 = User.create!(
+      name: 'username1',
+      photo: 'Photo',
+      bio: 'Text',
+      posts_counter: 2
+    )
+    @user2 = User.create!(
+      name: 'username2',
+      photo: 'Photo2',
+      bio: 'Text2',
+      posts_counter: 0
+    )
+    @post1 = Post.create!(
+      title: 'Title1',
+      text: 'Text1',
+      comments_counter: 3,
+      likes_counter: 1,
+      author: @user1
+    )
+    @post2 = Post.create!(
+      title: 'Title2',
+      text: 'Text2',
+      comments_counter: 0,
+      likes_counter: 0,
+      author: @user1
+    )
+    @comment1 = Comment.create!(
+      text: 'Comment1',
+      post: @post1,
+      author: @user2
+    )
+    @comment2 = Comment.create!(
+      text: 'Comment2',
+      post: @post1,
+      author: @user2
+    )
+    @comment3 = Comment.create!(
+      text: 'Comment3',
+      post: @post1,
+      author: @user2
+    )
 
-        visit user_post_path(@user_1.id, @post_1.id)
-    end 
+    visit user_post_path(@user1.id, @post1.id)
+  end
 
-    it 'Show the post title' do
-        expect(page).to have_content('Title1')
-    end
+  it 'Show the post title' do
+    expect(page).to have_content('Title1')
+  end
 
-    it 'Show the post author' do
-        expect(page).to have_content('username1')
-    end
+  it 'Show the post author' do
+    expect(page).to have_content('username1')
+  end
 
-    it 'Show the number of comments' do
-        expect(page).to have_content('3')
-    end
+  it 'Show the number of comments' do
+    expect(page).to have_content('3')
+  end
 
-    it 'Show the number of likes' do
-        expect(page).to have_content('1')
-    end
+  it 'Show the number of likes' do
+    expect(page).to have_content('1')
+  end
 
-    it 'Show the post body' do 
-        expect(page).to have_content('MyText1')
-    end
+  it 'Show the post body' do
+    expect(page).to have_content('MyText1')
+  end
 
-    it 'Show the comment author' do
-        expect(page).to have_content('username2')
-    end
+  it 'Show the comment author' do
+    expect(page).to have_content('username2')
+  end
 
-    it 'Show the comment body' do
-        expect(page).to have_content('Comment1')
-        expect(page).to have_content('Comment2')
-        expect(page).to have_content('Comment3')
-    end
+  it 'Show the comment body' do
+    expect(page).to have_content('Comment1')
+    expect(page).to have_content('Comment2')
+    expect(page).to have_content('Comment3')
+  end
 end
