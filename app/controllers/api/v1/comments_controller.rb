55b46667ebb6af5e59120694b604c7330933b358
haliljon <juraboevhaliljon@gmail.com>
2023-04-22 19:29:36 +0500

    Fix linters

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,40 +1,40 @@
 class Api::V1::CommentsController < ApplicationController
-    def index
-        @comments = Comment.all
-        render json: @comments
-    end
+  def index
+    @comments = Comment.all
+    render json: @comments
+  end
 
-    def show
-        @comment = Comment.find(params[:id])
-        render json: @comment
-    end
+  def show
+    @comment = Comment.find(params[:id])
+    render json: @comment
+  end
 
-    def create
-        @comment = Comment.new(comment_params)
-        if @comment.save
-            render json: @comment, status: :created, location: @comment
-        else
-            render json: @comment.errors, status: :unprocessable_entity
-        end
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      render json: @comment, status: :created, location: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
     end
+  end
 
-    def update
-        @comment = Comment.find(params[:id])
-        if @comment.update(comment_params)
-            render json: @comment
-        else
-            render json: @comment.errors, status: :unprocessable_entity
-        end
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      render json: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
     end
+  end
 
-    def destroy
-        @comment = Comment.find(params[:id])
-        @comment.destroy
-    end
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+  end
 
-    private
+  private
 
-    def comment_params
-        params.require(:comment).permit(:user_id)
-    end
+  def comment_params
+    params.require(:comment).permit(:user_id)
+  end
 end
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,52 +1,52 @@
 class Api::V1::PostsController < ApplicationController
-    before_action :set_post, only: [:show, :update, :destroy]
+  before_action :set_post, only: %i[show update destroy]
 
-    # GET /posts
-    def index
-        @posts = Post.all
+  # GET /posts
+  def index
+    @posts = Post.all
 
-        render json: @posts
+    render json: @posts
+  end
+
+  # GET /posts/1
+  def show
+    render json: @post
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render json: @post, status: :created, location: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
     end
+  end
 
-    # GET /posts/1
-    def show
-        render json: @post
+  # PATCH/PUT /posts/1
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
     end
+  end
 
-    # POST /posts
-    def create
-        @post = Post.new(post_params)
+  # DELETE /posts/1
+  def destroy
+    @post.destroy
+  end
 
-        if @post.save
-            render json: @post, status: :created, location: @post
-        else
-            render json: @post.errors, status: :unprocessable_entity
-        end
-    end
+  private
 
-    # PATCH/PUT /posts/1
-    def update
-        if @post.update(post_params)
-            render json: @post
-        else
-            render json: @post.errors, status: :unprocessable_entity
-        end
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    # DELETE /posts/1
-    def destroy
-        @post.destroy
-    end
-
-    private
-
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-        @post = Post.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def post_params
-        params.require(:post).permit(:user_id)
-    end
+  # Only allow a trusted parameter "white list" through.
+  def post_params
+    params.require(:post).permit(:user_id)
+  end
 end
